d30345b465b9f9a0e8ebce15800ae142c15d82f4
mszewczyk <szewczykm94@gmail.com>
2017-11-18 19:38:59 +0100

    Doc fix

--- service-etcd-registrator/src/main/java/com/szewczyk/microservices/utils/core/Registration.java
+++ service-etcd-registrator/src/main/java/com/szewczyk/microservices/utils/core/Registration.java
@@ -3,7 +3,7 @@ package com.szewczyk.microservices.utils.core;
 public interface Registration {
     /*****
      * Based on fact that in custom network each docker microservice ip address is available in DNS under it's container id
-     * we can simply add container id to etcd with for discovery purposes.
+     * we can simply add container id to etcd with it's name for discovery purposes.
      * returns true if succeeded
      * else false
      */
